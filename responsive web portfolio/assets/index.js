a64f4a9148e65fa607ae02c17420ed899e26c2f3
harwellkaleb <harwellkaleb@gmail.com>
2023-01-15 13:42:43 -0600

    pt. 4

--- responsive web portfolio/assets/index.js
+++ responsive web portfolio/assets/index.js
@@ -31,28 +31,23 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
-const skillsContent = document.getElementsByClassName('skills_content')
-const skillsHeader = document.querySelectorAll('.skills_header')
+const skillsContent = document.getElementsByClassName('skills_content'),
+      skillsHeader = document.querySelectorAll('.skills_header')
 
-      function toggleSkills(){
-        let itemClass = this.parentNode.ClassName
+function toggleSkills(){
+    let itemClass = this.parentNode.className
 
-        for(i = 0; i < skillsContent.length; i++){
-            skillsContent[i].className = 'skills_content skills close'
-        }
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills_content skills_close'
+    }
+    if(itemClass === 'skills_content skills_close'){
+        this.parentNode.className = 'skills_content skills_open'
+    }
+}
 
-        if(itemClass === 'skills_content skills_close'){
-            this.parentNode.className = 'skills_content skills_open'
-          }
-
-         
-      }
-
-      skillsHeader.forEach((Element)=>{
-        Element.addEventListener('click',toggleSkills)
-      })
-
-  
+skillsHeader.forEach((el) =>{
+    el.addEventListener('click', toggleSkills)
+})
 
 /*==================== QUALIFICATION TABS ====================*/
 
