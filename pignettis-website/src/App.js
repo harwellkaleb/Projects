8efdbea26d2794b8df73454d47fd29c4a8cedc67
harwellkaleb <harwellkaleb@gmail.com>
2023-02-09 14:46:42 -0600

    pg1

--- pignettis-website/src/App.js
+++ pignettis-website/src/App.js
@@ -3,11 +3,9 @@ import React from 'react';
 import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
 import { Navbar } from './components';
 import './App.css';
-import Main from './components/Main';
 
 const App = () => (
   <div>
-    <Main />
     <Navbar />
     <Header />
     <AboutUs />
@@ -17,7 +15,7 @@ const App = () => (
     <Laurels />
     <Gallery />
     <FindUs />
-    <Footer />
+    <Footer /> 
   </div>
 );
 
--- pignettis-website/src/container/Chef/Chef.jsx
+++ pignettis-website/src/container/Chef/Chef.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
+
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className='app__bg app__wrapper section__padding'>
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt='chef' />
+    </div>
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's word" />
+      <h1 className='headtext__cormorant'>
+        What we believe in
+      </h1>
+    </div>
   </div>
 );
 
