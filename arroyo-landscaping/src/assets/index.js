c31d4c71b8c178257c08e9a1ee8ab1d20a5c5d59
harwellkaleb <harwellkaleb@gmail.com>
2023-03-04 03:18:30 -0600

    webhero

--- arroyo-landscaping/src/assets/index.js
+++ arroyo-landscaping/src/assets/index.js
@@ -24,6 +24,7 @@ import people01 from "./people01.png";
 import people02 from "./people02.png";
 import people03 from "./people03.png";
 import treegrass from "./treegrass.png";
+import sprout from "./sprout.svg";
 
 export {
   airbnb,
@@ -52,4 +53,5 @@ export {
   people02,
   people03,
   treegrass,
+  sprout,
 };
--- arroyo-landscaping/src/components/Hero.jsx
+++ arroyo-landscaping/src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import styles from "../style";
-import { treegrass,discount } from "../assets";
+import { treegrass,sprout } from "../assets";
 import GetStarted from "./GetStarted";
 
 const Hero = () => {
@@ -7,10 +7,10 @@ const Hero = () => {
     <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
       <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
         <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
-          <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
+          <img src={sprout} alt="discount" className="w-[32px] h-[32px]" />
           <p className={`${styles.paragraph} ml-2`}>
-            <span className="text-white">20%</span> Discount For{" "}
-            <span className="text-white">1 Month</span> Account
+            <span className="text-white">Get Your </span> Free{" "}
+            <span className="text-white">Estimate </span> Today !
           </p>
         </div>
 
